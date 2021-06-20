4a86e8d071ebbbbb8cdc3fc7b24f5eb08802c016
Samul333 <samulshrestha101@gmail.com>
2021-06-20 19:15:42 +0545

    Hello there

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-app.use(cors())
+
 const server = require('http').Server(app)
-const io = require('socket.io')(server)
+const io = require('socket.io')(server,{
+	cors: {
+		origin: "*",
+		methods: [ "GET", "POST" ]
+	}
+})
 const { ExpressPeerServer } = require('peer');
 const peerServer = ExpressPeerServer(server, {
   debug: true
 });
 const { v4: uuidV4 } = require('uuid')
-
+app.use(cors())
 app.use('/peerjs', peerServer);
 
 app.set('view engine', 'ejs')
