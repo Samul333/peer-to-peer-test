a11f932027b6144a51d8dd0efde0a7127cc3ed2d
Samul333 <samulshrestha101@gmail.com>
2021-06-20 19:08:37 +0545

    dev branch

--- public/script.js
+++ public/script.js
@@ -1,9 +1,9 @@
-const socket = io();
+const socket = io('/');
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined,{
   path:'/peerjs',
   host: '/', 
-  port:'443'
+  port:443
 })
 let myVideoStream;
 const myVideo = document.createElement('video')
