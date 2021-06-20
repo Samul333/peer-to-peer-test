45141058921b463022d9f25c954e9cc7942db822
Samul333 <samulshrestha101@gmail.com>
2021-06-20 18:41:42 +0545

    added

--- public/script.js
+++ public/script.js
@@ -1,6 +1,10 @@
 const socket = io();
 const videoGrid = document.getElementById('video-grid')
-const myPeer = new Peer(undefined)
+const myPeer = new Peer(undefined,{
+  secure: true, 
+  host: 'https://peer-to-peer-test.herokuapp.com/', 
+  port: 443,
+})
 let myVideoStream;
 const myVideo = document.createElement('video')
 myVideo.muted = true;
